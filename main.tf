4dbeb27a96bb9bfc9c6105c930fd68428dd443ef
Ornella-Diana <wouanjio@gmail.com>
2025-09-02 19:55:53 +0200

    ec2 terraform

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "terraform_ornella" {
+  ami                    = "ami-00ca32bbc84273381"
+  instance_type          = "t2.small"
+  key_name               = "ornella"
+
+  tags = {
+    Name = "terra-ornella-v1"
+  }
+}
