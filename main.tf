84c8e3bafd20268195c9942133404cb1bb590aa3
Ornella-Diana <wouanjio@gmail.com>
2025-09-03 20:51:08 +0200

    Main mofified

--- main.tf
+++ main.tf
@@ -2,25 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-# Generate an SSH key pair locally
-resource "tls_private_key" "terra_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
 
-# Create AWS key pair using the public key
-resource "aws_key_pair" "terra_keypair" {
-  key_name   = "${var.instance_name}-key"
-  public_key = tls_private_key.terra_key.public_key_openssh
-}
 
-# Save private key locally (so you can SSH later)
-resource "local_file" "terra_private_key" {
-  content         = tls_private_key.terra_key.private_key_pem
-  filename        = "${path.module}/${var.instance_name}-key.pem"
-  file_permission = "0600"
-}
-
+# 
 # Create EC2 instance
 resource "aws_instance" "terra_instance" {
   ami           = var.ami_id
